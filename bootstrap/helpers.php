e74da10f1d835880cacc16c678a0ff500b4864d4
young <yangp67@sina.com>
2018-04-21 01:02:01 +0000

    modify error about get_db_config

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -7,7 +7,7 @@ function get_db_config()
 
         return $db_config = [
             'connection' => 'pgsql',
-            'host'=> $usrl['host'],
+            'host'=> $url['host'],
             'database' => substr($url['path'],1),
             'username' => $url['user'],
             'password' => $url['pass'],
